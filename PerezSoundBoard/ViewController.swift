d078f1ab2e5b5574d0ae94bc787ed405e6180f06
joseperezLuque <jose.perez.l@tecsup.edu.pe>
2024-05-18 19:47:31 -0500

    Initial Commit

---
+++ PerezSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PerezSoundBoard
+//
+//  Created by Jose Adriano Perez Luque on 18/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
